da42d87e6a16f36ad905185ed9ad27220c14e532
USER-20170721YY\Administrator <1046171507@qq.com>
2018-01-31 13:41:35 +0800

    获取target

--- src/components/vuex.vue
+++ src/components/vuex.vue
@@ -2,6 +2,7 @@
 	<div class="vuex">
 		<h3>状态管理</h3>
 		<link-component></link-component>
+		<p><span @click="click($event)">点击此处span后面的input聚焦</span><input type="text" /></p>
 		<router-view name="left"></router-view>
 		<router-view name="right"></router-view>
 	</div>
@@ -9,6 +10,12 @@
 <script>
 	export default {
 		name: 'hello',
+		methods: {
+			click(event) {
+				var $input = event.currentTarget.nextElementSibling;
+				$input.focus();
+			}
+		}
 	}
 </script>
 
